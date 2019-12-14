f625d31be1260808e128d33a15caf7ea0b9f0b1d
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2019-12-14 18:22:30 +0100

    FIX prediction issue when negative ETA

--- src/app.rs
+++ src/app.rs
@@ -80,12 +80,12 @@ impl <'a> UI <'a> {
                     target: Option<f64>) {
         if let Some(target) = target {
             let ETA: f64 = (target - regression.1) / regression.0;
-            let remaining_time = ETA - min_time;
+            let remaining_time = (ETA - max_time).max(0.0);
             let completion_percentage = (max_time / ETA * 100.0) as i16;
             let gauge_title= &format!(
                 "Estimated time of completion: {:.*}s (remaining: {:.*}s)",
-                2, if ETA < 0.0 { INFINITY } else { ETA },
-                2, if ETA < 0.0 { INFINITY } else { (ETA - max_time).max(0.0) }
+                2, if remaining_time <= 0.0 { INFINITY } else { ETA },
+                2, if remaining_time <= 0.0 { INFINITY } else { remaining_time }
             );
             let mut gauge_size = frame.size();
             gauge_size.height = frame.size().height / 10;
