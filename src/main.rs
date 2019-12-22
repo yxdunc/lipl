ab3a6ec00d27eed7faaef1e11256899324a2b060
irabeson <IohannRabeson@users.noreply.github.com>
2019-12-22 15:06:28 -0500

    Use a monotonic clock to control the frame rate.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use structopt::StructOpt;
-use std::time::{SystemTime, UNIX_EPOCH};
-use termion::async_stdin;
-use termion::input::TermRead;
 use app::UI;
 use core::time;
-
+use std::time::Instant;
+use structopt::StructOpt;
+use termion::async_stdin;
+use termion::input::TermRead;
 
 mod app;
 mod plotting_utils;
-mod widget_progress_bar;
 mod widget_main_chart;
+mod widget_progress_bar;
 mod widget_text_output;
-
-static ONE_BILLION: f32 = 1000000000.0;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -36,26 +33,28 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    let mut last_time = 0.0;
-    let refresh_rate = time::Duration::from_nanos((args.refresh_rate * ONE_BILLION) as u64);
+    let refresh_rate = time::Duration::from_secs_f32(args.refresh_rate);
     let mut ui = UI::new(
         &args.command,
         args.target_result,
         args.history_len,
         args.show_regression_line.or(Some(true)).unwrap(),
-        args.show_target_line.or(Some(false)).unwrap()
+        args.show_target_line.or(Some(false)).unwrap(),
     );
     let mut keys = async_stdin().keys();
+    // Substract refresh_rate to display the first frame immediatly
+    let mut last_time = Instant::now() - refresh_rate;
 
-    loop{
-        let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs_f64();
-        if refresh_rate.as_secs_f64() < current_time - last_time {
+    loop {
+        let current_time = Instant::now();
+        if refresh_rate < current_time - last_time {
             ui.evaluate();
             last_time = current_time;
         }
         if !ui.event_handler(keys.next()) {
-            ui.clean_up_terminal();
-            break
+            break;
         }
-    }  
+    }
+
+    ui.clean_up_terminal();
 }
