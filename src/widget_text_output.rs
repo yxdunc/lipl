f78a7d3eb026328e991eaca53c9ee2e52e3a77a1
irabeson <IohannRabeson@users.noreply.github.com>
2019-12-22 17:39:29 -0500

    The option returned by Vec::last() is None if the Vec is empty

--- src/widget_text_output.rs
+++ src/widget_text_output.rs
@@ -1,17 +1,17 @@
-use tui::Frame;
-use tui::backend::TermionBackend;
-use termion::raw::RawTerminal;
-use std::io::Stdout;
-use tui::widgets::{Paragraph, Text, Block, Widget};
 use std::borrow::Cow;
+use std::io::Stdout;
+use termion::raw::RawTerminal;
+use tui::backend::TermionBackend;
 use tui::layout::Alignment;
+use tui::widgets::{Block, Paragraph, Text, Widget};
+use tui::Frame;
 
 pub fn text_output(frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>, text_result_history: &Vec<(f64, String)>) {
-    if !text_result_history.is_empty() {
-        let last_result = text_result_history.last().unwrap();
-
-        Paragraph::new(vec!(Text::Raw(Cow::from(&last_result.1))).iter())
-            .block(Block::default().title(&format!("Elapsed time: {:.*} seconds", 2, last_result.0)))
+    if let Some(last_result) = text_result_history.last() {
+        Paragraph::new(vec![Text::Raw(Cow::from(&last_result.1))].iter())
+            .block(
+                Block::default().title(&format!("Elapsed time: {:.*} seconds", 2, last_result.0)),
+            )
             .alignment(Alignment::Left)
             .render(frame, frame.size());
     }
