cc35535ffdd8d90378f2b914f42eda0f2b5a5572
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2019-12-23 02:58:53 +0100

    FIX most warnings raised by clippy

--- src/widget_text_output.rs
+++ src/widget_text_output.rs
@@ -6,7 +6,7 @@ use tui::layout::Alignment;
 use tui::widgets::{Block, Paragraph, Text, Widget};
 use tui::Frame;
 
-pub fn text_output(frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>, text_result_history: &Vec<(f64, String)>) {
+pub fn text_output(frame: &mut Frame<TermionBackend<RawTerminal<Stdout>>>, text_result_history: &[(f64, String)]) {
     if let Some(last_result) = text_result_history.last() {
         Paragraph::new(vec![Text::Raw(Cow::from(&last_result.1))].iter())
             .block(
